0dc0bd26dabfbdb022dec9ee300d8db0e49f8009
Light12092006 <jgcfabris12@gmail.com>
2025-03-18 16:47:25 -0300

    Update aula5.js

--- aula5/aula5.js
+++ aula5/aula5.js
@@ -1,11 +1,10 @@
-
-console.log("OI");
-
-//let nome = prompt("Qual o seu nome?");
-//console.log(nome);
-
-let i = 0 ;
-while(i < 10){
-    console.log(i);
-    i++;
+for(let a = 1; a < 100; a+= 2){
+    console.log(a)
 }
+
+console.log("Exericio 2")
+for(let i = 5; i <= 500; i += 5){
+    console.log(i);
+}
+
+let a = prompt("Digite um nuemero: ")
