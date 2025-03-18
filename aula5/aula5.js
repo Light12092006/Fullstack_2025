38f617eb6c9e7ccf1c44360def7516eba3838068
Light12092006 <jgcfabris12@gmail.com>
2025-03-18 17:02:28 -0300

    Update aula5.js

--- aula5/aula5.js
+++ aula5/aula5.js
@@ -1,10 +1,20 @@
-for(let a = 1; a < 100; a+= 2){
-    console.log(a)
-}
+//for(let a = 1; a < 100; a+= 2){
+  //  console.log(a)
+//}
 
-console.log("Exericio 2")
-for(let i = 5; i <= 500; i += 5){
-    console.log(i);
-}
+//console.log("Exericio 2")
+//for(let i = 5; i <= 500; i += 5){
+//    console.log(i);
+//}
 
-let a = prompt("Digite um nuemero: ")
+//let a = parseIntprompt("Digite um nuemero: ")
+//for(a; a > 0; a-=1){
+   // console.log(a)
+//}
+
+let mult = 1
+let x = parseInt(prompt("Digite um numero: "));
+for(let i = x; i > 0; i--){
+    mult *= i;
+    console.log(i,mult);
+}
