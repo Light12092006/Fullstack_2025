7e0e0318b681e77c2261dbd392ca6ecc2474bc64
Light12092006 <jgcfabris12@gmail.com>
2025-04-29 14:36:18 -0300

    servidor

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,14 +1,87 @@
-var http = require('http');
+require("colors");
+var http = require ("http");
+var express = require ("express");
+let bodyParser = require("body-parser")
 
-var express = require('express');
-
-var app = express();
-
+var app = express ();
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var server = http.createServer(app);
-
 server.listen(80);
 
-console.log("Servidor rodando")
+console.log("Servidor Rodando..." .rainbow);
 
+
+app.get("/inicio",function(requisicao,resposta){
+    resposta.redirect("exercicio/Home.html")
+})
+
+app.post("/inicio", function(requisicao,resposta){
+    resposta.redirect("exercicio/Home.html")
+})
+
+// app.get("/cadastrar", function(requisicao,resposta){
+//     let nome = requisicao.query.nome;
+//     let login = requisicao.query.login;
+//     let senha = requisicao.query.senha;
+//     let nasc = requisicao.query.nascimento;
+
+//     console.log(nome,login,senha,nasc)
+    
+// })
+
+
+
+
+app.post("/cadastrar", function(requisicao,resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+
+    console.log(nome,login,senha,nasc)
+    
+    var data = { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha };
+
+    resposta.render("resposta",{nome,login,senha,nasc});
+    
+}) 
+
+app.get("/for_ejs",function(requisicao,resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
+
+// ----------------------------------------------------------------------------------------------------------------
+
+app.post("/cadastrolab", function(requisicao,resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+
+    console.log(nome,login,senha,nasc)
+
+    resposta.render("resposta",{nome,login,senha,nasc});
+    
+}) 
+
+app.get("/for_ejs",function(requisicao,resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
+
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const uri ='mongodb+srv://jgcfabris12:tomate12@joao.5bzjezq.mongodb.net/?retryWrites=true&w=majority&appName=Joao';
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("Joao");
+var usuarios = dbo.createCollection("usuarios",);
